f17378ee0b1b20e0543d2fb32d7f1460981b743b
Raimon <raimon.txollo@gmail.com>
2025-03-18 09:29:18 +0100

    Fix header and footer styles

--- src/data/data.ts
+++
@@ -1,6 +0,0 @@
-import { Board } from "./types.js";
-
-const board: Board = {
-  columnsTotal: 3,
-  rowsTotal: 3,
-};
--- src/data/types.ts
+++
@@ -1,9 +0,0 @@
-export type Cell = {
-  hasMine: boolean;
-  adjacentMinesTotal: number;
-};
-
-export type Board = {
-  rowsTotal: number;
-  columnsTotal: number;
-};
