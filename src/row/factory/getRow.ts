d59e88ab0b2c25064340855319af70b513b1a657
Raimon <raimon.txollo@gmail.com>
2025-03-20 20:33:16 +0100

    Refactor row module with types, getRow function and tests

---
+++ src/row/factory/getRow.ts
@@ -0,0 +1,18 @@
+import { Row } from "../types";
+import getCell from "../../cell/factory/getCell";
+
+export const getRow = (cellsNumber: number): Row => {
+  const cellsNumberInteger = Math.round(cellsNumber);
+
+  const row: Row = [];
+
+  if (cellsNumberInteger < 3) {
+    throw new Error("Columns minimum number must be 3");
+  }
+
+  for (let column = 0; column < cellsNumberInteger; column++) {
+    row[column] = getCell();
+  }
+
+  return row;
+};
