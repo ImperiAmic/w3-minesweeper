d02127d8a7d165132ab0b27632e50681192ac085
Raimon <raimon.txollo@gmail.com>
2025-03-23 20:17:13 +0100

    Add renderGameOver function to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { setAdjacentMinesTotal } from "./board/adjacent-mines/setAdjacentMinesTotal.js";
 import { getBoard } from "./board/factory/getBoard.js";
-import { renderAllMines } from "./board/ui/renderAllMines.js";
+import { renderGameOver } from "./board/ui/renderGameOver.js";
 import { renderBoard } from "./board/ui/renderBoard.js";
 
 const board = getBoard(6);
 
 renderBoard(setAdjacentMinesTotal(board));
-renderAllMines();
+renderGameOver();
